635131088757cddef28c541debfcfa3dbd19761c
Lave Gupta <lave.gupta@synacor.com>
2020-12-21 16:21:39 +0530

    FIX_TEMPLATE: fix build failure in config.yml and import withZimletIntlWrapper insted of withIntlWrapper

--- template/src/enhancers.js
+++ template/src/enhancers.js
@@ -1,14 +1,14 @@
-import { withIntlWrapper } from '@zimbra-client/enhancers';
+import { withZimletIntlWrapper } from '@zimbra-client/enhancers';
 
 /**
- * @method withIntlWrapper
+ * @method withZimletIntlWrapper
  * accepts three arguments which can be use to load zimlet locale.
  * @param {Object} - with following values
  * @param {Function} importFn which returns `import` with intl locale path of the zimlet.
  * @param {Boolean} showLoader Show loader on container or not
  *
  */
-export const withIntl = () => withIntlWrapper(
+export const withIntl = () => withZimletIntlWrapper(
     {
         importFn: locale => import(/* webpackMode: "eager" */`./intl/${locale}.json`),
         showLoader: false
